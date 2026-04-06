ea0673d90e514c0eac38ab9a995ef6294cbd7599
Adham Abbas <57569827+adhamabbas@users.noreply.github.com>
2026-04-06 10:44:31 +0200

    Update baseURL for API calls

--- src/Services/apiCalls.js
+++ src/Services/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const baseURL = "https://alalamya.onrender.com/api/v1/";
-const baseURL = "https://alalamya.onrender.com/api/v1/";
+const baseURL = "https://alalamya-t42y.onrender.com/api/v1/";
 
 
 export const postData = async (url, data, token) => {
